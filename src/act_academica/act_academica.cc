5ebcf66d3e64913da6d11d061e18f033ca67436e
Carlos de la Torre Frias <p12tofrc@uco.es>
2023-12-07 19:26:00 +0100

    ActAcademica 1.0

---
+++ src/act_academica/act_academica.cc
@@ -0,0 +1,31 @@
+#include "act_academica.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+act_academica::act_academica(std::string titulo, std::string descripcion, float precio, int aforomax){
+    titulo_=titulo;
+    descripcion_=descripcion;
+    precio_= precio;
+    aforomax_=aforomax;
+}
+
+act_academica::SetId(){
+    std::ifstream DataAct("../BD/ActAcademicas.txt");
+    if(DataAct.is_open()){
+        std::string linea;
+        std::streampos posIn= DataAct.tellg();
+        DataAct.seekg(0,std::ios::end);
+        std::streampos posFin= DataAct.tellg();
+        std::streamsize finArchivo= posFin-posIn;
+        DataAct.seekg(-finArchivo,std::ios::end);
+        while(std::getline(DataAct, linea)){
+            std::istringstream ss(linea);
+            ss.ignore(1);
+            int idef;
+            std::getline(ss,idef,'|');
+            id_= ++ifed;
+        }
+        DataAct.close();
+    }
+}
---
+++ src/act_academica/act_academica.h
@@ -0,0 +1,32 @@
+#ifndef ACTACADEMICA_H
+#define ACTACADEMICA_H
+#include "lista_asistencia.h"
+#include <iostream>
+#include <string>
+
+class act_academica{
+    private:
+        int id_;
+        std::string titulo_;
+        std::string descripcion_;
+        float precio_;
+        int aforomax_;
+    public:
+        act_academica(std::string titulo, std::string descripcion, float precio, int aforomax);
+        //Getters
+        int GetId(){return id_;}
+        std::string GetTitulo(){return titulo_;}
+        std::string GetDesc(){return descripcion_;}
+        float GetPrecio(){return precio_;}
+        int GetAforoMax(){return aforomax_;}
+        //Setters
+        void SetId();
+        void SetTitulo(std::string titulo){titulo_= titulo;}
+        void SetDesc(std::string descripcion){descripcion_= descripcion;}
+        void SetPrecio(float precio){precio_= precio;}
+        void SetAforoMax(int aforomax){aforomax_= aforomax;}
+        //Funciones Propias
+};
+
+
+#endif
