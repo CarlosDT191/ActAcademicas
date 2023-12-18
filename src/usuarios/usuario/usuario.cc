b40d2f9e9834203fe26dd98913e54c40a00f754a
Daniel Grande Rubio <lrg200175@gmail.com>
2023-12-18 18:00:37 +0100

    Usuario 1.3

--- src/usuarios/usuario/usuario.cc
+++ src/usuarios/usuario/usuario.cc
@@ -16,53 +16,58 @@ bool Usuario::Login(){
         //Pedir el correo
         std::cout << "Introduzca su correo (todo en minúscula): ";
         std::cin >> correo;
-        if (correo.size() == 15 && correo.compare(correo.size()-7, 7, "@uco.es") == 0){
-            correo_= correo;
-            //Abrir el archivo de cuentas
-            std::ifstream cuentasFile("../BD/cuentas.txt");
-            if (!cuentasFile.is_open()) {
-                std::cout << "Error al abrir el archivo de cuentas." << std::endl;
-                return false;
-            }
-
-            // Buscar el correo en el archivo
-            std::string line;
-            bool CorrectPassword = false;
-            while (std::getline(cuentasFile, line)) {
-                // Suponemos que el formato del archivo es "|Correo|Contraseña"
-                std::istringstream ss(line);
-                std::string l_correo;
-                std::string l_password;
-                ss.ignore(1); 
-                std::getline(ss, l_correo, '|');
-                if(l_correo == correo_){
-                    std::getline(ss, l_password, '|');
-                    for (int i = 0; i < 3 && CorrectPassword == false; i++){
-                        std::cout <<"Escriba la contraseña:" << std::endl;
-                        std::cin >>password;
-                        if (l_password == password){
-                            password_ = password;
-                            std::getline(ss, rol_, '|');
-                            std::getline(ss, carrera_, '|');
-                            cuentasFile.close();
-                            return true;
-                        }
-                        else {
-                            std::cout <<"Tienes: " << 2-i << " intentos" << std::endl;
-                        }
-                    }
-                    cuentasFile.close();
+        if(correo.size() == 15){
+            if (correo.compare(correo.size()-7, 7, "@uco.es") == 0){
+                correo_= correo;
+                //Abrir el archivo de cuentas
+                std::ifstream DataCuentas("src/BD/cuentas.txt");
+                if (!DataCuentas.is_open()) {
+                    std::cout << "Error al abrir el archivo de cuentas." << std::endl;
                     return false;
                 }
+
+                // Buscar el correo en el archivo
+                std::string linea;
+                bool CorrectPassword = false;
+                while (std::getline(DataCuentas, linea)) {
+                    // Suponemos que el formato del archivo es "|Correo|Contraseña"
+                    std::istringstream ss(linea);
+                    std::string l_correo;
+                    std::string l_password;
+                    ss.ignore(1); 
+                    std::getline(ss, l_correo, '|');
+                    if(l_correo == correo_){
+                        std::getline(ss, l_password, '|');
+                        for (int i = 0; i < 3 && CorrectPassword == false; i++){
+                            std::cout <<"Escriba la contraseña:" << std::endl;
+                            std::cin >>password;
+                            if (l_password == password){
+                                password_ = password;
+                                std::getline(ss, rol_, '|');
+                                std::getline(ss, carrera_, '|');
+                                DataCuentas.close();
+                                return true;
+                            }
+                            else {
+                                std::cout <<"Tienes: " << 2-i << " intentos" << std::endl;
+                            }
+                        }
+                        DataCuentas.close();
+                        return false;
+                    }
+                }
+                DataCuentas.close();
+                std::cout << "Usuario no encontrado" << std::endl;
+                std::cout << "Seleccione una opción \n 1. Seguir con el login. \n 2. Salir";
+                std::cin >> error;
             }
-            cuentasFile.close();
-            std::cout << "Usuario no encontrado" << std::endl;
-            return false;
+
         }
         else{
             std::cout << "El formato del correo consta de 15 caracteres, los ultimos 7 son @uco.es \n ¿Que desea hacer? \n 1. Continuar con login \n 2. Salir"<< std::endl;
             std::cin >> error;
         }
     }
+    return false;
 }
 
