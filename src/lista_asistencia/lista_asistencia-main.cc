0de98117620d4310011afab24ef0776f4f312a53
Carlos de la Torre Frias <p12tofrc@uco.es>
2023-12-18 21:01:08 +0100

    Lista Asistencia 1.2

---
+++ src/lista_asistencia/lista_asistencia-main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "lista_asistencia.h"
+
+int main(){
+    Lista_asistencia l1;
+
+    if(l1.RecogerListaAct(2)){
+        std::cout<<"ID: "<<l1.GetId_academica()<<"\n";
+        std::cout<<"Aforo restante: "<<l1.GetAforoRes()<<"\n";
+        std::vector<std::string> vec= l1.GetInscritos();
+        std::cout<<"INSCRITOS: ";
+        for(auto j: vec){
+            std::cout<<j<<" ";
+        }
+        std::cout<<"\n";
+    }
+
+    l1.AnadirAlumno(2,"p32lopem@uco.es");
+
+    std::cout<<"ID: "<<l1.GetId_academica()<<"\n";
+    std::cout<<"Aforo restante: "<<l1.GetAforoRes()<<"\n";
+    std::vector<std::string> vec= l1.GetInscritos();
+    std::cout<<"INSCRITOS: ";
+    for(auto i: vec){
+        std::cout<<i<<" ";
+    }
+    std::cout<<"\n";
+    
+    return 0;
+}
--- src/lista_asistencia/lista_asistencia.cc
+++ src/lista_asistencia/lista_asistencia.cc
@@ -2,14 +2,13 @@
 //Aqui estan las funciones de lista de asistencia (agregar alumno, eliminar alumno, leer y mostrar lista de asistencia)
 
 #include "lista_asistencia.h"
-#include "alumno.h"
 #include <fstream>
 #include <iostream>
 #include <sstream>
 
 
 bool Lista_asistencia::RecogerListaAct(int id_act){
-    std::istream ListData("../BD/ListaAsistencia.txt");
+    std::ifstream ListData("src/BD/ListaAsistencia.txt");
     std::string linea;
     std::getline(ListData, linea);
     while(std::getline(ListData, linea)){
@@ -20,11 +19,14 @@ bool Lista_asistencia::RecogerListaAct(int id_act){
         int id_A= std::stoi(id_A_str);
         if(id_act==id_A){
             id_act_= id_A;
-            std::string aforo_str;
+            std::string aforo_str, nombres;
             std::getline(ss,aforo_str,'|');
             aforo_restante_= std::stoi(aforo_str);
+            std::getline(ss,nombres,'|');
             std::string nombre_prov;
-            while(ss>>nombre_prov){
+            std::istringstream ss2(nombres);
+            vec_alum_.clear();
+            while(std::getline(ss2,nombre_prov,' ')){
                 vec_alum_.push_back(nombre_prov);
             }
             ListData.close();
@@ -38,12 +40,11 @@ bool Lista_asistencia::RecogerListaAct(int id_act){
 
 bool Lista_asistencia::AnadirAlumno(int id_act, std::string correo){
     RecogerListaAct(id_act);
-    
     aforo_restante_--;
     vec_alum_.push_back(correo);
 
-    std::ifstream ListData("../BD/ListaAsistencia.txt");
-    std::ofstream DataAux("../BD/aux.txt");
+    std::ifstream ListData("src/BD/ListaAsistencia.txt");
+    std::ofstream DataAux("src/BD/aux.txt");
     std::string linea;
 
     if(!(ListData.is_open()) || !(DataAux.is_open())){
@@ -51,43 +52,30 @@ bool Lista_asistencia::AnadirAlumno(int id_act, std::string correo){
         return false;
     }
 
+    std::getline(ListData,linea);
+    DataAux<<linea;
     while(std::getline(ListData,linea)){
         std::istringstream iss(linea);
         iss.ignore(1);
-        int id_prov;
-        std::getline(iss, id_prov);
+        std::string id_str;
+        std::getline(iss, id_str);
+        int id_prov= std::stoi(id_str);
         if(id_prov!= id_act_){
-            DataAux<<linea<<"\n";
+            DataAux<<"\n"<<linea;
         }
         else{
-            DataAux<<"|"<<id_act_<<"|"<<aforo_restante_<<"|";
+            DataAux<<"\n|"<<id_act_<<"|"<<aforo_restante_<<"|";
             for(auto j : vec_alum_){
                 DataAux<<j<<" ";
             }
-            DataAux<<"\n";
+            DataAux<<"|";
         }
     }
     
     DataAux.close();
     ListData.close();
 
-    remove("../BD/ListaAsistencia.txt");
-    rename("../BD/aux.txt","../BD/ListaAsistencia.txt");
+    remove("src/BD/ListaAsistencia.txt");
+    rename("src/BD/aux.txt","src/BD/ListaAsistencia.txt");
     return true;
 }
-
-
-
-
-
-
-
-
-// En vez de actualizar el fichero, se actualiza el vector de correos y lo metes directo en esa parte
-/*Crear una funcion booleana (true/false), donde se abra el fichero en modo lectura, cambiar modo escritura
-y escribo (preguntar primo), tiene que ir linea por linea y comparar el id de la actividad académica
-(este numero lo obtenemos de otra funcion) con el que hay en el fichero, una vez coincidan, tienes que meterlo en el
-ultimo apartado como si fuera un vector de nombres.*/
-
-
-
